b76ed1e670515eaa24831c884faecf8805e45d38
耿晓二号 <814707094@qq.com>
2020-11-03 10:47:52 +0800

    耿晓2号第一次提交

--- src/main/java/com/example/srpingbootvalidation/controller/TestController.java
+++ src/main/java/com/example/srpingbootvalidation/controller/TestController.java
@@ -28,7 +28,7 @@ public class TestController {
     @PostMapping("/batch/add/person")
     public String batchAddPerson(@RequestBody @ValidList List<Person> personList){
         System.out.println(personList.size());
-        return "ok";
+        return "ok222232315";
     }
 
 
